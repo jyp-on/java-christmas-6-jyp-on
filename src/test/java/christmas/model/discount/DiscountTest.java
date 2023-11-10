09db266579cc6c0c08ee495fc49bfe14841822ce
mythpoy <okmlnsunok@gmail.com>
2023-11-10 16:57:46 +0900

    test: 각 test간 의존성 제거

--- src/test/java/christmas/model/discount/DiscountTest.java
+++ src/test/java/christmas/model/discount/DiscountTest.java
@@ -2,83 +2,90 @@ package christmas.model.discount;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.model.menu.Appetizer;
-import christmas.model.menu.Dessert;
-import christmas.model.menu.Main;
-import christmas.model.menu.Menu;
-import java.util.ArrayList;
-import java.util.List;
+import christmas.model.Order;
+import christmas.util.ValidateUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 
 class DiscountTest {
 
-    List<Menu> menuList = new ArrayList<>();
+    @DisplayName("독립적인 테스트를 위한 전역 변수 초기화")
+    @BeforeEach
+    void cleanUp() {
+        Discount.EVENT_FLAG = true;
+        Discount.RECEIVE_FLAG = false;
+        Discount.TOTAL_DISCOUNT = 0;
+
+        ValidateUtil.menuNames.clear();
+        ValidateUtil.countMenu = 0;
+    }
 
     @Test
     void d_day_discount() {
-        menuList.add(Appetizer.양송이수프);
-        Discount maxDiscount = new Discount(25, menuList);
+        Order order = ValidateUtil.parseOrder("타파스-1,제로콜라-2");
+
+        Discount maxDiscount = new Discount(25, order);
         assertThat(maxDiscount.dDay()).isEqualTo(3400);
 
-        Discount minDiscount = new Discount(1, menuList);
+        Discount minDiscount = new Discount(1, order);
         assertThat(minDiscount.dDay()).isEqualTo(1000);
     }
 
     @Test
     void weekday_discount_with_dessert() {
-        menuList.add(Dessert.초코케이크);
+        Order order = ValidateUtil.parseOrder("초코케이크-2");
 
-        Discount friday = new Discount(1, menuList);
-        assertThat(friday.weekDay()).isEqualTo(2023);
+        Discount friday = new Discount(1, order);
+        assertThat(friday.weekDay()).isEqualTo(0);
 
-        Discount sunday = new Discount(3, menuList);
-        assertThat(sunday.weekDay()).isEqualTo(0);
+        Discount sunday = new Discount(3, order);
+        assertThat(sunday.weekDay()).isEqualTo(4046);
     }
 
     @Test
     void weekday_discount_without_dessert() {
-        menuList.add(Appetizer.타파스);
+        Order order = ValidateUtil.parseOrder("타파스-1");
 
-        Discount friday = new Discount(1, menuList);
+        Discount friday = new Discount(1, order);
         assertThat(friday.weekDay()).isEqualTo(0);
 
-        Discount sunday = new Discount(3, menuList);
+        Discount sunday = new Discount(3, order);
         assertThat(sunday.weekDay()).isEqualTo(0);
     }
 
     @Test
     void weekend_discount_with_main() {
-        menuList.add(Main.바비큐립);
+        Order order = ValidateUtil.parseOrder("바비큐립-1");
 
-        Discount friday = new Discount(3, menuList);
-        assertThat(friday.weekEnd()).isEqualTo(2023);
+        Discount friday = new Discount(3, order);
+        assertThat(friday.weekEnd()).isEqualTo(0);
 
-        Discount sunday = new Discount(8, menuList);
-        assertThat(sunday.weekEnd()).isEqualTo(0);
+        Discount sunday = new Discount(8, order);
+        assertThat(sunday.weekEnd()).isEqualTo(2023);
     }
 
     @Test
     void weekend_discount_without_main() {
-        menuList.add(Appetizer.타파스);
+        Order order = ValidateUtil.parseOrder("타파스-2");
 
-        Discount friday = new Discount(3, menuList);
+        Discount friday = new Discount(3, order);
         assertThat(friday.weekEnd()).isEqualTo(0);
 
-        Discount sunday = new Discount(8, menuList);
+        Discount sunday = new Discount(8, order);
         assertThat(sunday.weekEnd()).isEqualTo(0);
     }
 
     @Test
     void special_discount() {
-        Discount discount_1 = new Discount(3, menuList);
-        Discount discount_2 = new Discount(4, menuList);
-        Discount discount_3 = new Discount(25, menuList);
-        Discount discount_4 = new Discount(30, menuList);
+        Order order = ValidateUtil.parseOrder("타파스-2");
+
+        Discount discount_1 = new Discount(3, order);
+        Discount discount_2 = new Discount(4, order);
+        Discount discount_3 = new Discount(25, order);
 
         assertThat(discount_1.special()).isEqualTo(1000);
         assertThat(discount_2.special()).isEqualTo(0);
         assertThat(discount_3.special()).isEqualTo(1000);
-        assertThat(discount_4.special()).isEqualTo(0);
     }
 }

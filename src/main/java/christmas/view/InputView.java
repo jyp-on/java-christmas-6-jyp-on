1a520fb9749e9c601d0a1c1aa9775ffca864d24c
mythpoy <okmlnsunok@gmail.com>
2023-11-10 17:00:35 +0900

    refactor: Order 반환로직 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -20,7 +20,7 @@ public class InputView {
     public static Order readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
-        return new Order(ValidateUtil.parseOrder(input));
+        return ValidateUtil.parseOrder(input);
     }
 
 

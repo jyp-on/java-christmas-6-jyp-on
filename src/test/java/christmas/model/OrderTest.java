dc2704869cb5e54dc4c6e3a8f1aaf44307d61bf3
mythpoy <okmlnsunok@gmail.com>
2023-11-10 16:58:45 +0900

    test: 테스트 간 의존성 제거

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -2,19 +2,29 @@ package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.model.menu.Menu;
+import christmas.model.discount.Discount;
 import christmas.util.ValidateUtil;
-import java.util.List;
-import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 class OrderTest {
 
+    @DisplayName("독립적인 테스트를 위한 전역 변수 초기화")
+    @BeforeEach
+    void cleanUp() {
+        Discount.EVENT_FLAG = true;
+        Discount.RECEIVE_FLAG = false;
+        Discount.TOTAL_DISCOUNT = 0;
+
+        ValidateUtil.menuNames.clear();
+        ValidateUtil.countMenu = 0;
+    }
+
     @Test
     void getTotalPrice() {
-        List<Map<Menu, Integer>> maps = ValidateUtil.parseOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
-        Order order = new Order(maps);
+        Order order = ValidateUtil.parseOrder("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
         int totalPrice = order.getTotalPrice();
         assertThat(totalPrice).isEqualTo(142000);
     }

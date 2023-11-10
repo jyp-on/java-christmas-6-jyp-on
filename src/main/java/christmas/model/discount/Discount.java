ced94e772b128a02fd1c0018fe87f5f0d5faff4c
mythpoy <okmlnsunok@gmail.com>
2023-11-10 16:57:30 +0900

    refactor: EVENT_FLAG가 false면 모든 혜택 없도록 변경

--- src/main/java/christmas/model/discount/Discount.java
+++ src/main/java/christmas/model/discount/Discount.java
@@ -1,28 +1,38 @@
 package christmas.model.discount;
 
+import christmas.model.Order;
 import christmas.model.menu.Dessert;
 import christmas.model.menu.Main;
 import christmas.model.menu.Menu;
 import java.util.List;
+import java.util.Map;
 
 public class Discount {
     private final int YEAR = 2023;
+    private final int date;
+    private final Order order;
+    public static int TOTAL_DISCOUNT = 0;
+    public static boolean EVENT_FLAG = true;
+    public static boolean RECEIVE_FLAG = false;
 
-    private int date;
-    private List<Menu> menuList;
-
-    public Discount(int _date, List<Menu> _menuList) {
+    public Discount(int _date, Order _order) {
         this.date = _date;
-        this.menuList = _menuList;
+        this.order = _order;
     }
 
     public int dDay() {
-        return 1000 + ((this.date - 1) * 100);
+        if (date < 26 && Discount.EVENT_FLAG) {
+            int discount = 1000 + ((this.date - 1) * 100);
+            TOTAL_DISCOUNT += discount;
+            return discount;
+        }
+        return 0;
     }
 
     public int special() {
         for (int i = 3; i <= 31; i += 7) {
-            if (date == 25 || date == i) {
+            if (date == 25 || date == i && Discount.EVENT_FLAG) {
+                TOTAL_DISCOUNT += 1000;
                 return 1000;
             }
         }
@@ -30,22 +40,41 @@ public class Discount {
     }
 
     public int weekDay() {
-        if (date % 7 <= 2 && date % 7 != 0 && findEnumInMenus(Dessert.class, menuList)) {
-            return menuList.size() * YEAR;
+        List<Map<Menu, Integer>> menuList = order.getOrder();
+
+        if ((date % 7 > 2 || date % 7 == 0) && findEnumInMenus(Dessert.class, menuList) && Discount.EVENT_FLAG) {
+            long count = menuList.stream()
+                    .flatMap(order -> order.entrySet().stream())
+                    .filter(entry -> entry.getKey() instanceof Dessert)
+                    .mapToLong(Map.Entry::getValue)
+                    .sum();
+
+            TOTAL_DISCOUNT += (int) (count * YEAR);
+            return (int) (count * YEAR);
         }
         return 0;
     }
 
     public int weekEnd() {
-        if ((date % 7 > 2 || date % 7 == 0) && findEnumInMenus(Main.class, menuList)) {
-            return menuList.size() * YEAR;
+        List<Map<Menu, Integer>> menuList = order.getOrder();
+
+        if ((date % 7 == 1 || date % 7 == 0) && findEnumInMenus(Main.class, menuList) && Discount.EVENT_FLAG) {
+            long count = menuList.stream()
+                    .flatMap(order -> order.entrySet().stream())
+                    .filter(entry -> entry.getKey() instanceof Main)
+                    .mapToLong(Map.Entry::getValue)
+                    .sum();
+
+            TOTAL_DISCOUNT += (int) (count * YEAR);
+            return (int) (count * YEAR);
         }
         return 0;
     }
 
     private <T extends Enum<T> & Menu> boolean findEnumInMenus(Class<T> enumClass,
-                                                               List<Menu> menuList) {
-        return menuList.stream()
+                                                               List<Map<Menu, Integer>> orderList) {
+        return orderList.stream()
+                .flatMap(order -> order.keySet().stream())
                 .anyMatch(enumClass::isInstance);
     }
 }
